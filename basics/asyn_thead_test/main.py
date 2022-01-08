19e45d563f4461ab377e093542c933c8a7f6d581
SMou <1197864339@qq.com>
2022-01-08 15:52:46 +0800

    1

---
+++ basics/asyn_thead_test/main.py
@@ -0,0 +1,32 @@
+import asyncio
+import time
+
+
+async def func1():
+    print("我是func1")
+    await asyncio.sleep(2)
+    print("func1结束")
+
+
+async def func2():
+    print("我是func2")
+    await asyncio.sleep(1)
+    print("func2结束")
+
+
+async def func3():
+    print("我是func3")
+    await asyncio.sleep(3)
+    print("func3结束")
+
+
+if __name__ == '__main__':
+    t = time.time()
+    f1 = func1()
+    f2 = func2()
+    f3 = func3()
+    tasks = [
+        f1, f2, f3,
+    ]
+    asyncio.run(asyncio.wait(tasks))
+    print(time.time()-t)
