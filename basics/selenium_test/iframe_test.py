8280b795532b5afeb358db53d8feaa3c2e6e0fdf
SMou <1197864339@qq.com>
2022-01-10 11:36:35 +0800

    windows

---
+++ basics/selenium_test/iframe_test.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+
+
+# web=Chrome()
+
+# web.get("http://www.wbdy.tv/play/30288_1_1.html")
+# iframe=web.find_element(By.XPATH,'//*[@id="mplay"]')
+# web.switch_to.frame(iframe)
+# input=web.find_element(By.XPATH,'//*[@id="dplayer"]/div[4]/div[1]/input')
+# placeholder=input.get_property('placeholder')
+# print(placeholder)
+# # web.switch_to.parent_frame()#跳到上层
+# web.close()
+
+
+
+
+
+
+
