1aef79f4391b35538918a415fea6c4af24e23247
SMou <1197864339@qq.com>
2022-02-21 14:50:19 +0800

    1

--- bobo/day07/main.py
+++ bobo/day07/main.py
@@ -4,3 +4,4 @@ import requests
 url = 'https://www.sogou.com/'
 res = requests.get(url=url)
 print(res.text)
+
--- bobo/day12/server.py
+++ bobo/day12/server.py
@@ -1,3 +1,6 @@
 # https://www.meijuw.com/vodplay/3985-1-1
 # 一级
 m1_url = "https://vod3.bdzybf3.com/20210608/JQtXiqqG/index.m3u8"
+# https://www.meijuw.com/vodplay/3985-1-1
+
+m1_url = "https://vod3.bdzybf3.com/20210608/JQtXiqqG/index.m3u8"
