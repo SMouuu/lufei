f73a8b440617bdc50710636f488e7f7420a37b12
SMou <1197864339@qq.com>
2022-02-21 14:47:57 +0800

    1

---
+++ bobo/day07/main.py
@@ -0,0 +1,6 @@
+import requests
+
+
+url = 'https://www.sogou.com/'
+res = requests.get(url=url)
+print(res.text)
---
+++ bobo/day12/server.py
@@ -0,0 +1,3 @@
+# https://www.meijuw.com/vodplay/3985-1-1
+# 一级
+m1_url = "https://vod3.bdzybf3.com/20210608/JQtXiqqG/index.m3u8"
